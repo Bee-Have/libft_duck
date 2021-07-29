04655953ec3e343bcbf5c3fa858d790395d68cb2
amarini- <amarini-@42.fr>
2021-07-29 18:19:04 +0200

    added security to freetab

--- Srcs/2D_Arrays/ft_freetab.c
+++ Srcs/2D_Arrays/ft_freetab.c
@@ -6,7 +6,7 @@
 /*   By: amarini- <amarini-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/07/29 15:51:18 by amarini-          #+#    #+#             */
-/*   Updated: 2021/07/29 15:54:29 by amarini-         ###   ########.fr       */
+/*   Updated: 2021/07/29 18:07:45 by amarini-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,8 @@ void	ft_freetab(char **tab)
 	int		i;
 
 	i = 0;
+	if (!tab)
+		return ;
 	while (tab[i])
 	{
 		free(tab[i]);
