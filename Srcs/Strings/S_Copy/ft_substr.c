9b7fa86d51c4993e354cc88de61475750c679314
amarini- <amarini-@42.fr>
2021-08-19 12:36:26 +0200

    forgot some printf inside substr

--- Srcs/Strings/S_Copy/ft_substr.c
+++ Srcs/Strings/S_Copy/ft_substr.c
@@ -6,7 +6,7 @@
 /*   By: amarini- <amarini-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/09/01 07:41:45 by amarini-          #+#    #+#             */
-/*   Updated: 2021/08/12 13:42:09 by amarini-         ###   ########.fr       */
+/*   Updated: 2021/08/19 12:35:56 by amarini-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,15 +32,9 @@ char	*ft_substr(char const *s, unsigned int start, size_t len)
 	if (!s)
 		return (NULL);
 	if (ft_strlen(s) == 0 || (int)start >= ft_strlen(s))
-	{
-		printf("should not\n");
 		return (return_empty());
-	}
 	if (ft_strlen(s) < (int)len)
-	{
-		printf("s_len[%d] len-[%d]\n", ft_strlen((const char *)s), (int)len);
 		result = (char *)malloc(ft_strlen(s) + 1 * sizeof(char));
-	}
 	else
 		result = (char *)malloc((len + 1) * sizeof(char));
 	if (!result)
