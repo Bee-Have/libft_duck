c643fb3e09ebfc016422290eb641155de4b8e8da
Bee-Have <duck.marini@gmail.com>
2022-01-26 19:51:26 +0100

    :bug: -Re-wroked ft_erase function, i still need to test what i did, so just test it in general

--- Srcs/2D_Arrays/ft_erase.c
+++ Srcs/2D_Arrays/ft_erase.c
@@ -6,7 +6,7 @@
 /*   By: user42 <user42@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/28 18:36:36 by amarini-          #+#    #+#             */
-/*   Updated: 2021/12/23 16:59:36 by user42           ###   ########.fr       */
+/*   Updated: 2022/01/26 16:40:59 by user42           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,7 @@ char	**ft_erase(char **str, int start, int amount)
 	int		length;
 	int		i;
 
-	i = start;
+	i = 0;
 	length = ft_tablen((const char **)str) - amount;
 	result = (char **)malloc((length + 1) * sizeof(char *));
 	if (!result)
@@ -26,8 +26,9 @@ char	**ft_erase(char **str, int start, int amount)
 	result[length] = NULL;
 	while (i < length)
 	{
-		result[i] = ft_strdup(str[amount + i]);
-		i++;
+		result[i] = ft_strdup(str[start]);
+		++i;
+		++start;
 	}
 	return (result);
 }
