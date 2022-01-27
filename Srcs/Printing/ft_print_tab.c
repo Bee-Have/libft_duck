30c73a3773327b78bed221f34544b2717f648e29
Albe MARINI-AUDOUARD <amarini-@e2r11p8.clusters.42paris.fr>
2022-01-27 18:30:45 +0100

    🎨🩹 -Added "(null)" print into ft_printab for the NULL terminating character in char**

--- Srcs/Printing/ft_print_tab.c
+++ Srcs/Printing/ft_print_tab.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_print_tab.c                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: user42 <user42@student.42.fr>              +#+  +:+       +#+        */
+/*   By: amarini- <amarini-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/29 16:39:22 by amarini-          #+#    #+#             */
-/*   Updated: 2021/12/23 17:01:35 by user42           ###   ########.fr       */
+/*   Updated: 2022/01/27 15:35:20 by amarini-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,12 +17,13 @@ void	ft_print_tab(char **tab)
 	int		row;
 
 	row = 0;
-	while (row < ft_tablen((const char **)tab))
+	while (tab[row])
 	{
 		write(1, tab[row], ft_strlen(tab[row]));
 		write(1, "\n", 1);
 		row++;
 	}
+	ft_putstr("(null)");
 	write(1, "\n", 1);
 	return ;
 }
