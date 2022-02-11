965ccead8444fed3e9f836c46fb87c083ba0d9e5
Albe MARINI-AUDOUARD <amarini-@e2r11p15.clusters.42paris.fr>
2022-02-11 12:01:12 +0100

    🐛 -Fixed tiny error in ft_insert_tab, turns out the way i was handling it wasn't great and it mae everything explode (it is now fixed :D)

--- Srcs/2D_Arrays/ft_insert_tab.c
+++ Srcs/2D_Arrays/ft_insert_tab.c
@@ -6,7 +6,7 @@
 /*   By: amarini- <amarini-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/10 14:28:36 by amarini-          #+#    #+#             */
-/*   Updated: 2022/02/10 19:54:12 by amarini-         ###   ########.fr       */
+/*   Updated: 2022/02/11 11:58:04 by amarini-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,8 +22,6 @@ char	**ft_insert_tab(char **arr, char *str, int index)
 	i = 0;
 	i_src = 0;
 	len = ft_tablen((const char **)arr) + 1;
-	if (arr[index] == NULL)
-		--len;
 	res = (char **)malloc((len + 1) * sizeof(char *));
 	if (!res)
 		return (NULL);
@@ -39,8 +37,6 @@ char	**ft_insert_tab(char **arr, char *str, int index)
 		}
 		++i;
 	}
-	if (arr[index] == NULL)
-		res[i] = ft_strdup(str);
 	ft_freetab(arr);
 	return (res);
 }
